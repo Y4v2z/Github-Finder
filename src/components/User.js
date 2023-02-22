2d715a9ca9ce0c2f0ddd2591963ab5970d10c78b
Yavuz Avci <yavuzavci01@gmail.com>
2023-02-22 11:04:46 +0200

    Fetch Api added

--- src/components/User.js
+++ src/components/User.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react'
 
 export class User extends Component {
     render() {
-        const { avatar_url, name, followers, github_url, website } = this.props.user; //User Sayfasında yer alan "user" tanımlamasının içinden gelen bilgi. Ör: aşağıda this.props.user.name yazmak yerine burada tanımlanır ve işlemler yapılır.
+        const { avatar_url, login, html_url } = this.props.user; //User Sayfasında yer alan "user" tanımlamasının içinden gelen bilgi. Ör: aşağıda this.props.user.name yazmak yerine burada tanımlanır ve işlemler yapılır.
         return (
             <div className="card mb-3">
                 <div className="row g-0">
                     <div className="col-md-3">
-                        <img className='img-fluid rounded-start avatar' src={avatar_url} alt={name} />
+                        <img className='img-fluid rounded-start avatar' src={avatar_url} alt={login} />
                     </div>
                     <div className="col-md-9">
                         <div className="card-body">
-                            <h3 className="card-title"> {name}</h3>
+                            <h3 className="card-title"> {login}</h3>
                             <hr />
-                            <p>Followers: {followers}</p>
-                            <a target="_blank" href={github_url} className="btn btn-primary">Github Profile</a>
-                            <a target="_blank" href={website} className="btn btn-primary ms-2">Website</a>
+                            <a target="_blank" rel="noreferrer" href={html_url} className="btn btn-primary">Github Profile</a>
                         </div>
                     </div>
                 </div>
